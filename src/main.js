88c3f502c3f9179750e4c8e2ed537d408ab429a3
Courtney Hancock <ceh.courtney@gmail.com>
2019-07-20 09:44:28 -0700

    Add icecream classes and methods, print to console

--- src/main.js
+++ src/main.js
@@ -4,3 +4,19 @@ class Desserts {
     this.calories = calories;
   }
 }
+
+class Icecream extends Desserts {
+  constructor(type, calories, flavor, scoops){
+    super(type, calories);
+    this.flavor = flavor;
+    this.scoops = scoops;
+  }
+  includeSpoon() {
+    console.log('Here is your spoon!');
+  }
+}
+
+const vanillaIcecream = new Icecream('Icecream', '340', 'Vanilla', '3');
+console.log(vanillaIcecream);
+
+vanillaIcecream.includeSpoon();
